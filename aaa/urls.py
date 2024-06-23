f9b33d7b900ea87200976a3106e4b9f9ae889542
r-srb <r.srb@email.cz>
2024-06-23 17:44:26 +0000

    tested win version

--- aaa/urls.py
+++ aaa/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('logout_complete/', views.logout_complete, name='logout_complete'),
     path('', views.index, name='index'),
 ]
