5da8aa1e202ca78abe10340b0799f53a5be14c74
r-srb <r.srb@email.cz>
2024-06-30 09:24:32 +0000

    Final AAA with IP subnet demo

--- aaa/admin.py
+++ aaa/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import DHCPRole
-# admin.site.register(DHCPRole)
 
-# from .models import GroupRoleMapping
-# admin.site.register(GroupRoleMapping)
+from .models import IPSubnet
+admin.site.register(IPSubnet)
