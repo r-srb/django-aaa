a016f3750763c1a0204afb2c32b3b97c87fd09bf
r-srb <r.srb@email.cz>
2024-06-30 08:39:10 +0000

    Initial version

--- aaa/admin.py
+++ aaa/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import IPSubnet
-admin.site.register(IPSubnet)
+# from .models import IPSubnet
+# admin.site.register(IPSubnet)
--- aaa/models.py
+++ aaa/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-from django.contrib.auth.models import Group
+# from django.contrib.auth.models import Group
 
-class IPSubnet(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    subnet = models.CharField(max_length=255)
+# class IPSubnet(models.Model):
+#     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+#     subnet = models.CharField(max_length=255)
 
-    def __str__(self):
-        return f"{self.group.name}: {self.subnet}"
+#     def __str__(self):
+#         return f"{self.group.name}: {self.subnet}"
