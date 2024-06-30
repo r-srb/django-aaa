a967165835c767e28dd28c1de615a495679912c8
r-srb <r.srb@email.cz>
2024-06-30 07:59:51 +0000

    AAA with IP subnets demo

--- aaa/models.py
+++ aaa/models.py
@@ -2,24 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-# from django.contrib.auth.models import Group
-# 
-# class DHCPRole(models.Model):
-#     role = models.CharField(max_length=255, unique=True)
-#     group_name = models.ForeignKey(Group, on_delete=models.CASCADE)
-#     ip_subnet = models.CharField(max_length=50)  # IP subnet ve formátu CIDR (např. 192.168.1.0/24)
-
-#     def __str__(self):
-#         # return f'{self.group_name} -> {self.role.name}'
-#         # return f'{self.role} -> {self.group_name.name}'
-#         return self.ip_subnet
-
 from django.contrib.auth.models import Group
 
-# class GroupRoleMapping(models.Model):
-#     role = models.CharField(max_length=255, unique=True)
-#     group = models.ForeignKey(Group, on_delete=models.CASCADE)
-#     ip_subnet = models.CharField(max_length=50)  # IP subnet in CIDR format (ex. 192.168.1.0/24)
+class IPSubnet(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    subnet = models.CharField(max_length=255)
 
-#     def __str__(self):
-#         return f'{self.role} -> {self.group.name}'
+    def __str__(self):
+        return f"{self.group.name}: {self.subnet}"
--- aaa/views.py
+++ aaa/views.py
@@ -6,6 +6,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from django.contrib.auth.models import Group
+from .models import IPSubnet
+import ipaddress
+
+
 # Login pres HTML stranku:
 def login_view(request):
     if request.method == 'POST':
@@ -16,7 +21,7 @@ def login_view(request):
             login(request, user)
             return redirect(reverse('index'))
         else:
-            return HttpResponse('Could not verify your access level for that URL.\nYou have to login with proper credentials', status=401)
+            return HttpResponse('Could not verify your access level for that URL.\nYou have to <a href="/">login</a> with proper credentials', status=401)
     return render(request, 'login.html')
 
 # def login_view(request):
@@ -56,6 +61,64 @@ def logout_view(request):
     # response['WWW-Authenticate'] = 'Basic realm="Login Required"'
     return response
 
+# @login_required
+# def index(request):
+#     return HttpResponse("Hello world!<br><a href='logout/'>Logout<a> " + request.user.username)
+
 @login_required
 def index(request):
-    return HttpResponse("Hello world!<br><a href='logout/'>Logout<a> " + request.user.username)
+    subnets = ["10.1.0.0/16", "10.2.0.0/16", "10.2.3.0/24"]
+    # Get the user's groups
+    user_groups = request.user.groups.all()
+
+    # Get the IPSubnets for the user's groups
+    user_ip_subnets = IPSubnet.objects.filter(group__in=user_groups)
+
+    # Extract the subnets from the user's IPSubnets
+    user_subnet_list = [ipaddress.ip_network(ip_subnet.subnet) for ip_subnet in user_ip_subnets]
+
+    # Convert predefined subnets to ip_network objects
+    predefined_subnets = [ipaddress.ip_network(subnet) for subnet in subnets]
+
+    # Find the subnets that are in both the predefined subnets list and the user's subnet list
+    matching_subnets = [str(subnet) for subnet in predefined_subnets if any(subnet.subnet_of(user_subnet) for user_subnet in user_subnet_list)]
+
+    # Prepare the response
+    response = "Hello in " + ", ".join(matching_subnets) + " world!<br>"
+    response += f"<a href='logout/'>Logout</a> {request.user.username}"
+
+    return HttpResponse(response)
+
+# from django.contrib.auth.models import Group
+# from aaa.models import IPSubnet
+
+# group, created = Group.objects.get_or_create(name="AD:GL-SG-NetGlobalAdmins")
+# IPSubnet.objects.create(group=group, subnet="0.0.0.0/0")
+# group, created = Group.objects.get_or_create(name="AD:PL-SG-InfraAdmins")
+# IPSubnet.objects.create(group=group, subnet="10.16.0.0/12")
+# group, created = Group.objects.get_or_create(name="AD:CZ-SG-InfraAdmins")
+# IPSubnet.objects.create(group=group, subnet="10.32.0.0/12")
+# group, created = Group.objects.get_or_create(name="AD:RS-SG-InfraAdmins")
+# IPSubnet.objects.create(group=group, subnet="10.48.0.0/12")
+# group, created = Group.objects.get_or_create(name="AD:IT-SG-InfraAdmins")
+# IPSubnet.objects.create(group=group, subnet="10.64.0.0/12")
+# group, created = Group.objects.get_or_create(name="AD:RO-SG-InfraAdmins")
+# IPSubnet.objects.create(group=group, subnet="10.96.0.0/12")
+# group, created = Group.objects.get_or_create(name="AD:SK-SG-InfraAdmins")
+# IPSubnet.objects.create(group=group, subnet="10.112.0.0/12")
+
+# Generator:
+# while read gr ip; do
+# cat << EOF
+# group, created = Group.objects.get_or_create(name="AD:$gr")
+# IPSubnet.objects.create(group=group, subnet="$ip")
+# EOF
+# done << EOF
+# GL-SG-NetGlobalAdmins 0.0.0.0/0
+# PL-SG-InfraAdmins 10.16.0.0/12
+# CZ-SG-InfraAdmins 10.32.0.0/12
+# RS-SG-InfraAdmins 10.48.0.0/12
+# IT-SG-InfraAdmins 10.64.0.0/12
+# RO-SG-InfraAdmins 10.96.0.0/12
+# SK-SG-InfraAdmins 10.112.0.0/12
+# EOF
